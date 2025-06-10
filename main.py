0419cf73b9e28ab5452d707ec39cd25a60f7b4b0
A_Nightails <a.nightails@gmail.com>
2025-06-09 22:51:18 -0500

    Initial AI agent cli commit

---
+++ main.py
@@ -0,0 +1,44 @@
+import sys
+import os
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+
+def prompt_gemini(client, messages):
+    return client.models.generate_content(
+        model = 'gemini-2.0-flash-001',
+        contents = messages
+    )
+
+def display_response(user_prompt, response, verbose):
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print("Response:")
+    print(response.text)
+
+def main():
+    load_dotenv()
+
+    verbose = "--verbose" in sys.argv
+    args = list(filter(lambda x: not x.startswith("--"), sys.argv[1:]))
+
+    if not args:
+        print("Gemini Assistant")
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
+        sys.exit(1)
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    user_prompt = " ".join(args)
+    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
+    response = prompt_gemini(client, messages)
+
+    display_response(user_prompt, response, verbose)
+
+if __name__ == "__main__":
+    main()
+
