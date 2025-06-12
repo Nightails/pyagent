6c5ef9b77375b0621f6e8510014e9a87aca51f97
A_Nightails <a.nightails@gmail.com>
2025-06-12 10:30:42 -0500

    Add calculator and wip getting file function

---
+++ functions/get_files_info.py
@@ -0,0 +1,9 @@
+
+def get_files_info(working_directory, directory=None):
+    # get the working directory
+    # if directory not in working_directory:
+    #    return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    #if not directory:
+    #    return f'Error: "{directory}" is not a directory'
+    pass
+
