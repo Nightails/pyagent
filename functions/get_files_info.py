8e8ae4d0d830de20d27c07ec67a94f75b684b504
A_Nightails <a.nightails@gmail.com>
2025-06-12 22:36:52 -0500

    Clean code block organization

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -11,22 +11,17 @@ def get_files_info(working_directory, directory=None):
     if path.endswith('.'):
         path = path[:-1]
 
+    if not os.path.isdir(path):
+        return not_dir_error
     if not os.path.abspath(path).startswith(os.path.abspath(working_directory)):
         return outside_scope_error
 
-    is_dir = os.path.isdir(path)
-    is_file = os.path.isfile(path)
-    if is_dir:
-        path_content = os.listdir(path) if is_dir else []
-        content = ""
-        for item in path_content:
-            item_path = os.path.join(path, item)
-            if os.path.isdir(item_path):
-                content += f'{item}: file_size={os.path.getsize(item_path)}, is_dir=True\n'
-            elif os.path.isfile(item_path):
-                content += f'{item}: file_size={os.path.getsize(item_path)}, is_dir=False\n'
-        return content
-    elif is_file:
-        size = os.path.getsize(path)
-        return f'{path}: file_size={size}, is_dir={is_dir}'
-    return not_dir_error
+    path_content = os.listdir(path)
+    content = ""
+    for item in path_content:
+        item_path = os.path.join(path, item)
+        if os.path.isdir(item_path):
+            content += f'{item}: file_size={os.path.getsize(item_path)}, is_dir=True\n'
+        elif os.path.isfile(item_path):
+            content += f'{item}: file_size={os.path.getsize(item_path)}, is_dir=False\n'
+    return content
