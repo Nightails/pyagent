cd64b742e2300b4de588632dccafc8ea18625ceb
A_Nightails <a.nightails@gmail.com>
2025-06-15 10:05:36 -0500

    Agent able to iterrate the self prompt to complete task

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import os
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from config import MAX_ITER
 from prompts import SYSTEM_PROMPT
 from call_function import available_functions
 from call_function import function_names
+
 
 def prompt_gemini(client, messages):
     return client.models.generate_content(
@@ -16,21 +18,7 @@ def prompt_gemini(client, messages):
             system_instruction=SYSTEM_PROMPT),
     )
 
-def display_content(user_prompt, content, verbose):
-    if verbose:
-        print(f"User prompt: {user_prompt}")
-        print(f"Prompt tokens: {content.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {content.usage_metadata.candidates_token_count}")
-    if not content.function_calls:
-        return(content.text)
-    for call_part in content.function_calls:
-        function_response = call_function(call_part, verbose).parts[0].function_response.response
-        if function_response:
-            if verbose:
-                print(f"-> {function_response}")
-            return function_response["result"]
-        else:
-            raise Exception("Function failed to response")
+
 def call_function(function_call_part, verbose=False):
     function_name = function_call_part.name
     function_args = function_call_part.args
@@ -62,6 +50,39 @@ def call_function(function_call_part, verbose=False):
             ],
         )
 
+
+def iterate_gemini(client, user_prompt, verbose=False):
+    final_result = ""
+    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
+    iter_count = 0
+    while iter_count < MAX_ITER:
+        content = prompt_gemini(client, messages)
+        if verbose:
+                final_result += f"User prompt: {messages[iter_count].parts[0].text}"
+                final_result += f"\nPrompt tokens: {content.usage_metadata.prompt_token_count}"
+                final_result += f"\nResponse tokens: {content.usage_metadata.candidates_token_count}"
+        if not content.function_calls:
+            final_result += f"\n{content.text}"
+            break
+
+        for candidate in content.candidates:
+            messages.append(candidate.content)
+
+        for call_part in content.function_calls:
+            function_content = call_function(call_part, verbose)
+            messages.append(function_content)
+            function_response = function_content.parts[0].function_response.response
+            if function_response:
+                if verbose:
+                    final_result += f"\n-> {function_response}"
+                final_result += function_response["result"]
+            else:
+                break
+
+        iter_count += 1
+    return final_result
+
+
 def main():
     load_dotenv()
 
@@ -75,11 +96,9 @@ def main():
 
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-
     user_prompt = " ".join(args)
-    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
-    content = prompt_gemini(client, messages)
-    print(f"Response:\n{display_content(user_prompt, content, verbose)}")
+    print(iterate_gemini(client, user_prompt, verbose))
+
 
 if __name__ == "__main__":
     main()
